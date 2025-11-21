2c1d5e9d36588705e73b951303779c1be41f1c1c
liutao <liutaotao@mytijian.com>
2025-11-21 11:37:46 +0800

    feat: 将首页改为 loading 过渡页，自动跳转至 home

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,53 +1,22 @@
-<script lang="ts" setup>
-defineOptions({
-  name: 'Home',
-})
+<script setup lang="ts">
 definePage({
-  // 使用 type: "home" 属性设置首页，其他页面不需要设置，默认为page
-  type: 'home',
   style: {
-    // 'custom' 表示开启自定义导航栏，默认 'default'
     navigationStyle: 'custom',
-    navigationBarTitleText: '首页',
   },
 })
 
-const description = ref(
-  'unibest 是一个集成了多种工具和技术的 uniapp 开发模板，由 uniapp + Vue3 + Ts + Vite5 + UnoCss + VSCode 构建，模板具有代码提示、自动格式化、统一配置、代码片段等功能，并内置了许多常用的基本组件和基本功能，让你编写 uniapp 拥有 best 体验。',
-)
-console.log('index/index 首页打印了')
-
-onLoad(() => {
-  console.log('测试 uni API 自动引入: onLoad')
+onMounted(() => {
+  setTimeout(() => {
+    uni.reLaunch({ url: '/pages/home/index' })
+  }, 1000)
 })
 </script>
 
 <template>
-  <view class="bg-white px-4 pt-safe">
-    <view class="mt-10">
-      <image src="/static/logo.svg" alt="" class="mx-auto block h-28 w-28" />
-    </view>
-    <view class="mt-4 text-center text-4xl text-[#d14328]">
-      unibest
-    </view>
-    <view class="mb-8 mt-2 text-center text-2xl">
-      最好用的 uniapp 开发模板
-    </view>
-
-    <view class="m-auto mb-2 max-w-100 text-justify indent text-4">
-      {{ description }}
-    </view>
-    <view class="mt-4 text-center">
-      作者：
-      <text class="text-green-500">
-        菲鸽
-      </text>
-    </view>
-    <view class="mt-4 text-center">
-      官网地址：
-      <text class="text-green-500">
-        https://unibest.tech
-      </text>
+  <view class="h-screen w-full flex flex-col items-center justify-center bg-white">
+    <view class="i-carbon-circle-dash animate-spin text-4xl text-primary" />
+    <view class="mt-4 text-sm text-gray-500">
+      加载中...
     </view>
   </view>
 </template>
